c94651b3936c3ddb64b1d3bdf94b73b59fa49bd2
periclesmatos <spfcpericles@gmail.com>
2025-07-26 14:25:10 -0300

    feat: Adicioando RelatoriosProduto

--- src/SistemaVendasCLI.java
+++ src/SistemaVendasCLI.java
@@ -1,9 +1,9 @@
-import Service.RelatorioService;
 import model.Cliente;
 import model.Produto;
 import model.Venda;
 import model.Vendedor;
 import repository.DadosMock;
+import service.RelatoriosProduto;
 
 import java.util.List;
 import java.util.Scanner;
@@ -17,33 +17,61 @@ public class SistemaVendasCLI {
         List<Vendedor> vendedores = DadosMock.getVendedores();
         List<Venda> vendas = DadosMock.getVendas(produtos, clientes, vendedores);
 
-        RelatorioService relatorioService = new RelatorioService(vendas);
-
         Scanner scanner = new Scanner(System.in);
-        boolean comtinue = true;
-        while (comtinue) {
-            System.out.println("\nSistema de Vendas - Relatórios");
-            System.out.println("1. Ranking de vendedores por valor vendido");
-            System.out.println("2. Total de vendas por cidade do cliente");
-            System.out.println("3. Produto mais vendido por quantidade");
-            System.out.println("4. Ticket médio por vendedor");
-            System.out.println("5. Relatório mensal de vendas");
-            System.out.println("6. Clientes que mais compraram");
-            System.out.println("7. Sair");
-            System.out.print("Escolha uma opção: ");
+        int opcao;
+        do {
+            System.out.println("\n---- MENU DE RELATÓRIOS ----");
+            System.out.println("1. Realatorio de Produtos");
+            System.out.println("2. Relatorio de Clientes");
+            System.out.println("3. Relatorio de Vendas");
+            System.out.println("0. Sair");
 
-            int opcao = scanner.nextInt();
+            System.out.println("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            System.out.println("\n");
+
             switch (opcao) {
-                case 1 -> relatorioService.rankingVendedores();
-                case 7 -> {
+                case 1 -> relatorioProdutos(produtos);
+                case 2 -> System.out.println("Em desenvolvimento...");
+                case 3 -> System.out.println("Em desenvolvimento..");
+                case 0 -> {
                     System.out.println("Saindo...");
                     scanner.close();
-                    comtinue = false;
                 }
                 default -> System.out.println("Opção inválida, tente novamente.");
             }
-        }
+        } while (opcao != 0);
 
+    }
+
+    private static void relatorioProdutos(List<Produto> produtos) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+        do {
+            System.out.println("\n--- MENU ALUNO ---");
+            System.out.println("1. Listar produtos ordenados por nome");
+            System.out.println("2. Filtrar produtos com estoque abaixo de 20");
+            System.out.println("3. Listar nomes dos produtos");
+            System.out.println("4. Listar nomes da categoria 'Papelaria'");
+            System.out.println("5. Calcular valor total do estoque");
+            System.out.println("6. Listar os 3 produtos mais caros");
+
+            System.out.println("0. Voltar");
+
+            System.out.println("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1 -> RelatoriosProduto.listarProdutosOrdenadosPorNome(produtos);
+                case 2 -> RelatoriosProduto.filtrarEstoqueAbaixoDe20(produtos);
+                case 3 -> RelatoriosProduto.listarNomesProdutos(produtos);
+                case 4 -> RelatoriosProduto.listarNomesProdutosCategoriaPapelaria(produtos);
+                case 5 -> RelatoriosProduto.calcularValorTotalEstoque(produtos);
+                case 6 -> RelatoriosProduto.listarTop3ProdutosPreco(produtos);
+                case 0 -> System.out.println("Voltando ao menu principal...");
+                default -> System.out.println("Opção inválida.");
+            }
+        } while (opcao != 0);
     }
 
 }
---
+++ src/service/RelatoriosProduto.java
@@ -0,0 +1,49 @@
+package service;
+
+import model.Produto;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class RelatoriosProduto {
+
+    public static void listarProdutosOrdenadosPorNome(List<Produto> produtos) {
+        produtos.stream()
+                .sorted(Comparator.comparing(Produto::getNome))
+                .forEach(System.out::println);
+    }
+
+    public static void filtrarEstoqueAbaixoDe20(List<Produto> produtos) {
+        produtos.stream()
+                .filter(p -> p.getQuantidadeEstoque() < 20)
+                .forEach(System.out::println);
+    }
+
+    public static void listarNomesProdutos(List<Produto> produtos) {
+        produtos.stream()
+                .map(Produto::getNome)
+                .forEach(System.out::println);
+    }
+
+    public static void listarNomesProdutosCategoriaPapelaria(List<Produto> produtos) {
+        produtos.stream()
+                .filter(p -> p.getCategoria().equals("papelaria"))
+                .map(Produto::getNome)
+                .forEach(System.out::println);
+    }
+
+    public static void calcularValorTotalEstoque(List<Produto> produtos) {
+        int quantiadeTotalEstoque = produtos.stream()
+                .map(Produto::getQuantidadeEstoque)
+                .reduce(0, Integer::sum);
+
+        System.out.println("Total em estoque: " + quantiadeTotalEstoque);
+    }
+
+    public static void listarTop3ProdutosPreco(List<Produto> produtos) {
+        produtos.stream()
+                .sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .limit(3)
+                .forEach(System.out::println);
+    }
+}
